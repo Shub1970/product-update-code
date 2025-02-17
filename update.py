e90f90b8875a815b2053c04a61faff6d96fe9dab
Shub1970 <18shubham1998@gmail.com>
2025-02-17 09:23:19 +0530

    chane in main

--- update.py
+++ update.py
@@ -87,6 +87,7 @@ for item in data:
         # this is the change in branch1
         if product_response.status_code in [200, 201]:
             print(f"Product {item['name']} created successfully.")
+            # change in main
         else:
             print(f"Failed to create product {
                   item['name']}: {product_response.text}")
